17411c67005b28bdb791b672b6774d43cdd0cda6
Roman Amin <mankhan35@gmail.com>
2018-11-22 08:31:44 -0800

    Listing Page Without Model(Database)

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('about',views.about, name='about'),
+    path('about/',views.about, name='about'),
 ]
