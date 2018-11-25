f51cf0a3206a5d8ed131910457a1ce1ba89b6bfc
Roman Amin <mankhan35@gmail.com>
2018-11-25 22:54:00 +0500

    Model Added Feild Garage in listing Model

--- listings/models.py
+++ listings/models.py
@@ -14,6 +14,7 @@ class Listing(models.Model):
     bedrooms = models.IntegerField()
     bathroom= models.DecimalField(max_digits=2, decimal_places=1)
     bedrooms = models.IntegerField(default=0)
+    garage = models.IntegerField(default=0) 
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
